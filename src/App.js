c0b6a738a0745e7a59dad98ee48ad9dd653213eb
Wan Nabil <wannabil.xyz@gmail.com>
2023-05-03 22:20:50 +0800

    Add existing file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+     {
+        <><div>
+          <div>{count}</div>
+          </div><button className="btn btn-success mx-3" onClick={() => setCount(count + 1)}>Increase by 1</button>
+          <button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)}>Decrease by 1</button>
+          <button className="btn btn-secondary mx-3" onClick={() => setCount(0)}>Reset to 0</button></>
+
+    }
+         
       </header>
     </div>
   );
